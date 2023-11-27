32faee700905d13d47c152637242ace542f11a10
Fireshadow111 <muhammadrajah16@gmail.com>
2023-11-27 12:02:34 +0200

    adding

--- script/index.js
+++ script/index.js
@@ -1,4 +1,4 @@
-// 
+// declaring variables
 let minMaxOutput = document.getElementById('min-maxOutput');
 let calButton1 = document.getElementById('calButton1');
 let calButton2 = document.getElementById('calButton2');
@@ -8,13 +8,13 @@ let minMaxArray = [4, 7, 111];
 
 // function to calculate min value
 function min() {
-    //using a rest operator to loop through every value in the array
+    //using a rest parameter to loop through every value in the array
     result = Math.min(...minMaxArray);
     minMaxOutput.value = result;
 }
 // function to calculate max value
 function max() {
-    //using a rest operator to loop through every value in the array
+    //using a rest parameter to loop through every value in the array
     result = Math.max(...minMaxArray);
     minMaxOutput.value = result;
 }
